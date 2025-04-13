3da781243caefd39fcad91f4e5156bce2009f543
Gimle Larpes <gimlelarpes@gmail.com>
2025-04-13 17:17:53 +0200

    rough stuff

--- RemoveBG.py
+++ RemoveBG.py
@@ -1,3 +1,5 @@
+## EXAMPLE - ALMOST WORKING
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -5,152 +7,235 @@
 # with an option to process all open images.
 # Original author: James Huang <elastic192@gmail.com>
 # Modified by: Tech Archive <medium.com/@techarchive>
-# Date: 13/9/24
+# Rewritten by: Gimle Larpes <gimlelarpes@gmail.com>
+# Date: 13/4/25
 
-from gimpfu import *
-import os
-import tempfile
+import sys
+import os, string, tempfile
 import platform
-import subprocess
+import gi
+gi.require_version('Gimp', '3.0')
+from gi.repository import Gimp
+gi.require_version('GimpUi', '3.0')
+from gi.repository import GimpUi
+from gi.repository import GLib
+from gi.repository import GObject
+from gi.repository import Gio
 
-tupleModel = (
-    "u2net",
-    "u2net_human_seg",
-    "u2net_cloth_seg",
-    "u2netp",
-    "silueta",
-    "isnet-general-use",
-    "isnet-anime",
-    "sam"
-)
 
-def remove_background(image, drawable, asMask, selModel, AlphaMatting, aeValue, make_square):
+def N_(message): return message
+def _(message): return GLib.dgettext(None, message)
+    
+tupleModel = ("u2net","u2net_human_seg", "u2net_cloth_seg", "u2netp", "silueta", "isnet-general-use", "isnet-anime")
+
+def pdbCall(procedureName, args):
+    pdb_proc   = Gimp.get_pdb().lookup_procedure(procedureName)
+    pdb_config = pdb_proc.create_config()
+    for x in args:
+        pdb_config.set_property(x, args[x])
+    return pdb_proc.run(pdb_config)
+
+def store_layer_png(image, drawable, path):
+    interlace, compression = 0, 2
+
+    _, x, y = drawable.get_offsets()
+    width, height = drawable.get_width(), drawable.get_height()
+    tmp_img = Gimp.Image.new(width, height, image.get_base_type())
+    drawable.set_offsets(0, 0)
+    tmp_layer = Gimp.Layer.new_from_drawable (drawable, tmp_img)
+    tmp_img.insert_layer (tmp_layer, None, 0)
+    drawable.set_offsets(x, y)
+    
+    args = {
+      "run-mode": Gimp.RunMode.NONINTERACTIVE,
+      "image": tmp_img,
+      "file": Gio.File.new_for_path(path),
+      "options": None,
+      "interlaced": interlace,
+      "compression": compression,
+      "bkgd": True,
+      "offs": False,
+      "phys": True,
+      "time": True,
+      "save-transparent": True
+    }
+    pdbCall('file-png-export', args)
+
+    tmp_img.delete()
+    
+def store_layer_jpg(image, drawable, path):
+    _, x, y = drawable.get_offsets()
+    width, height = drawable.get_width(), drawable.get_height()
+    tmp_img = Gimp.Image.new(width, height, image.get_base_type())
+    drawable.set_offsets(0, 0)
+    tmp_layer = Gimp.Layer.new_from_drawable (drawable, tmp_img)
+    tmp_img.insert_layer (tmp_layer, None, 0)
+    drawable.set_offsets(x, y)
+    
+    args = {
+      "run-mode": Gimp.RunMode.NONINTERACTIVE,
+      "image": tmp_img,
+      "file": Gio.File.new_for_path(path),
+      "options": None,
+      "quality": 0.95,
+      "smoothing": 0,
+      "optimize": True
+    }
+    pdbCall('file-jpeg-export', args)
+    
+    tmp_img.delete()
+
+def RemoveBG(procedure, run_mode, image, drawables, config, data):
+    if run_mode == Gimp.RunMode.INTERACTIVE:
+        GimpUi.init('plug-in-RemoveBG-python')
+
+        dialog = GimpUi.ProcedureDialog(procedure=procedure, config=config)
+        # dialog.fill(None)
+        dialog.fill(["asMask"])
+        dialog.fill(["AlphaMatting"])
+        dialog.fill(["aeValue"])
+        dialog.get_int_combo("selModel", GimpUi.IntStore.new (tupleModel))
+        dialog.fill(["selModel"])
+        if not dialog.run():
+            return procedure.new_return_values(Gimp.PDBStatusType.CANCEL, GLib.Error())
+            
     removeTmpFile = True
+    OutputMessage = False
+    osName = platform.system()
+    exportSep = str(os.sep)
     tdir = tempfile.gettempdir()
-    jpgFile = os.path.join(tdir, "Temp-gimp-0000.jpg")
-    pngFile = os.path.join(tdir, "Temp-gimp-0000.png")
-
-    image.undo_group_start()
-    curLayer = pdb.gimp_image_get_active_layer(image)
-    x1, y1 = curLayer.offsets
-
-    # Save the current layer to a temporary JPEG file
-    pdb.file_jpeg_save(image, curLayer, jpgFile, jpgFile, 0.95, 0, 1, 0, "", 0, 1, 0, 0)
-
-    # Path to your Python executable where rembg is installed
-    pythonExe = "C:\\Users\\adamy\\AppData\\Local\\Programs\\Python\\Python312\\python.exe"
-
-    # Build the rembg command using rembg.cli
-    cmd = [
-        pythonExe, '-m', 'rembg.cli', 'i', '-m', tupleModel[selModel]
-    ]
+    if OutputMessage:
+        Gimp.message(tdir)
+    jpgFile = "%s%sTemp-gimp-0000.jpg" % (tdir, exportSep)
+    pngFile = "%s%sTemp-gimp-0000.png" % (tdir, exportSep)
+    errFile = "%s%sErrMsg.txt" % (tdir, exportSep)
+    x1 = 0
+    y1 = 0
+    option = ""
+    
+    Gimp.progress_init("AI Remove image background ...")
+    Gimp.progress_update(0.0)
+            
+    asMask       = config.get_property('asMask')
+    AlphaMatting = config.get_property('AlphaMatting')
+    aeValue      = config.get_property('aeValue')
+    selModel     = config.get_property('selModel')
+    
+    if OutputMessage:
+        msg = "data: %s %s %d %s" % (asMask, AlphaMatting, aeValue, tupleModel[selModel])
+        Gimp.message(msg)
+    
+    aiExe = "C:\\Users\\gimle\\AppData\\Local\\Programs\\Python\\Python310\\Scripts\\rembg.exe" # NOT WORKING?
+    # HAS TO BE INSTALLED IN: "If you want to use any Python modules, other than the standard library, you have to bundle them with your plugin. You can install them in the plugin folder with pip install --target /path/to/plugin somepackage. You can use the site module to add additional paths to the Python search path. "
+    if OutputMessage:
+        Gimp.message(aiExe)
     if AlphaMatting:
-        cmd.extend(['-a', '-ae', str(aeValue)])
-    cmd.extend([jpgFile, pngFile])
+        option = "-a -ae %d" % (aeValue)
+        
+    cmd = '""%s" i -m %s %s "%s" "%s""' % (aiExe, tupleModel[selModel], option, jpgFile, pngFile)
+    if OutputMessage:
+        Gimp.message(cmd)
+        
+    Gimp.progress_update(0.1)
+    
+    Gimp.context_push()
+    image.undo_group_start()
+    
+    drawable = drawables[0]
+    curLayer = drawable
+    _, x1, y1 = drawable.get_offsets()
+    Gimp.progress_update(0.2)
+    
+    store_layer_jpg(image, drawable, jpgFile)
+    Gimp.progress_update(0.4)
+    
+    ret = os.system(cmd + ' 2> ' + errFile)
+    Gimp.progress_update(0.8)
+    if OutputMessage:
+        if os.path.exists(errFile):
+            fp = open(errFile, "r")
+            output = fp.read()
+            # if len(output) > 0:
+                # unicodestr = output.decode('cp950').encode('utf-8')
+            fp.close()
+            os.remove(errFile)
+            if len(output) > 0:
+                # Gimp.message(unicodestr)
+                Gimp.message(output)
 
-    # Execute the command and capture output using subprocess.Popen
-    try:
-        process = subprocess.Popen(
-            cmd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=False
-        )
-        stdout, stderr = process.communicate()
-        if process.returncode != 0:
-            pdb.gimp_message("rembg error:\n" + stderr.decode('utf-8'))
-            image.undo_group_end()
-            return
-    except Exception as e:
-        pdb.gimp_message("Failed to execute rembg:\n" + str(e))
-        image.undo_group_end()
-        return
-
-    # Load the output PNG as a new layer
-    if os.path.exists(pngFile):
-        newlayer = pdb.gimp_file_load_layer(image, pngFile)
-        pdb.gimp_image_add_layer(image, newlayer, 0)
-        pdb.gimp_layer_set_offsets(newlayer, x1, y1)
-
-        if asMask:
-            # Create and add mask if the option is selected
-            mask = pdb.gimp_layer_create_mask(newlayer, ADD_ALPHA_MASK)
-            pdb.gimp_layer_add_mask(newlayer, mask)
+    Gimp.progress_update(0.9)
+    file_exists = os.path.exists(pngFile)
+    if file_exists:
+        args = {
+          "run-mode": Gimp.RunMode.NONINTERACTIVE,
+          "image": image,
+          "file": Gio.File.new_for_path(pngFile)
+        }
+        result = pdbCall('gimp-file-load-layer', args);
+        if (result.index(0) == Gimp.PDBStatusType.SUCCESS):
+            gimp_layer = result.index(1)
+            gimp_layer.set_offsets(x1, y1)
+            image.insert_layer(gimp_layer, drawable.get_parent(),
+                           image.get_item_position(drawable))
+            if asMask:
+                image.select_item(2, gimp_layer)
+                image.remove_layer(gimp_layer)
+                copyLayer = Gimp.Layer.copy(curLayer)
+                image.insert_layer(copyLayer, drawable.get_parent(),
+                           image.get_item_position(drawable))
+                mask=copyLayer.create_mask(4)
+                copyLayer.add_mask(mask)
+                Gimp.Selection.none(image)
         else:
-            # No mask option selected:
-            # Step 1: Remove the original layer
-            pdb.gimp_image_remove_layer(image, curLayer)
-
-            # Step 2: Create a new white background layer
-            white_bg_layer = pdb.gimp_layer_new(image, image.width, image.height, RGB_IMAGE, "White Background", 100, LAYER_MODE_NORMAL)
-            pdb.gimp_drawable_fill(white_bg_layer, WHITE_FILL)
-
-            # Step 3: Ensure the white background is at the bottom, newlayer on top
-            pdb.gimp_image_insert_layer(image, white_bg_layer, None, -1)
-            pdb.gimp_image_raise_item_to_top(image, newlayer)
-
-            # Step 4: Merge the new transparent layer with the white background
-            merged_layer = pdb.gimp_image_merge_down(image, newlayer, CLIP_TO_BOTTOM_LAYER)
-
-            # Ensure visibility of the final merged layer
-            pdb.gimp_item_set_visible(merged_layer, True)
-
-        # Step 5: Handle the "Make Square" option
-        if make_square:
-            # Get the current width and height of the image
-            img_width = pdb.gimp_image_width(image)
-            img_height = pdb.gimp_image_height(image)
-
-            # Determine the longer side (either width or height)
-            max_side = max(img_width, img_height)
-
-            # Resize the canvas to make the image square
-            pdb.gimp_image_resize(image, max_side, max_side, (max_side - img_width) // 2, (max_side - img_height) // 2)
-
-        # Step 6: Flatten the image (merge all visible layers)
-        pdb.gimp_image_merge_visible_layers(image, CLIP_TO_BOTTOM_LAYER)
-
-    else:
-        pdb.gimp_message("Output PNG file was not created.")
-
+            print("Error loading layer from openraster image.")
+    
+    Gimp.displays_flush()
     image.undo_group_end()
-    gimp.displays_flush()
-
-    # Clean up temporary files
+    Gimp.context_pop()
+    
     if removeTmpFile:
-        try:
-            os.remove(jpgFile)
-            os.remove(pngFile)
-        except Exception:
-            pass
+        if osName == "Windows":
+            del_command = "del \"%s%sTemp-gimp-0000.*\"" % (tdir, exportSep)
+        else:
+            del_command = "rm %s%sTemp-gimp-0000.*" % (tdir, exportSep)
+        os.system(del_command)
 
-def python_fu_RemoveBG(image, drawable, asMask, selModel, AlphaMatting, aeValue, make_square, process_all_images):
-    if process_all_images:
-        images = gimp.image_list()
-        for img in images:
-            drawable = pdb.gimp_image_get_active_layer(img)
-            remove_background(img, drawable, asMask, selModel, AlphaMatting, aeValue, make_square)
-    else:
-        remove_background(image, drawable, asMask, selModel, AlphaMatting, aeValue, make_square)
+    Gimp.progress_update(1.0)
+    Gimp.progress_end()
+    
+    return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
 
-register(
-    "python_fu_RemoveBG",
-    "AI Remove image background",
-    "Remove image backgrounds using AI with an option to process all open images.",
-    "Tech Archive",
-    "GPLv3",
-    "2023",
-    "<Image>/Python-Fu/AI Remove Background...",
-    "RGB*, GRAY*",
-    [
-        (PF_TOGGLE, "asMask", "Use as Mask", True),
-        (PF_OPTION, "selModel", "Model", 0, tupleModel),
-        (PF_TOGGLE, "AlphaMatting", "Alpha Matting", False),
-        (PF_SPINNER, "aeValue", "Alpha Matting Erode Size", 15, (1, 100, 1)),
-        (PF_TOGGLE, "make_square", "Make Square", False),
-        (PF_TOGGLE, "process_all_images", "Process all open images", False)
-    ],
-    [],
-    python_fu_RemoveBG)
+class AIRemoveBG (Gimp.PlugIn): # START HERE, DEBUG
+    def do_query_procedures(self):
+        return [ "plug-in-RemoveBG-python" ]
 
-main()
+    def do_set_i18n (self, name):
+        return False
+
+    def do_create_procedure(self, name):
+        procedure = Gimp.ImageProcedure.new(self, name,
+                                            Gimp.PDBProcType.PLUGIN,
+                                            RemoveBG, None)
+
+        procedure.set_image_types("*")
+
+        procedure.set_menu_label("AI Remove image background ...")
+        procedure.add_menu_path('<Image>/Tools/')
+
+        procedure.set_documentation("AI Remove image background",
+                                    "Remove image backgrounds using AI with an option to process all open images.",
+                                    name)
+        procedure.set_attribution("Gimle Larpes", "Gimle Larpes", "2025")
+        
+        procedure.add_boolean_argument ("asMask", _("As mask"), _("longer desc"),
+                                       True, GObject.ParamFlags.READWRITE)
+        procedure.add_boolean_argument ("AlphaMatting", _("alpha matting"), _("alpha matting"),
+                                       False, GObject.ParamFlags.READWRITE)
+        procedure.add_int_argument ("aeValue", _("ALPHA_MATTING_ERODE_SIZE"), _("ALPHA_MATTING_ERODE_SIZE"),
+                                       1, 100, 15, GObject.ParamFlags.READWRITE)
+        procedure.add_int_argument ("selModel", _("Model"), _("longer desc"),
+                                       0, len(tupleModel), 0, GObject.ParamFlags.READWRITE)
+        
+        return procedure
+
+Gimp.main(AIRemoveBG.__gtype__, sys.argv)
